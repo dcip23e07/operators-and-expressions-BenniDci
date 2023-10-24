fbd1c07a688ce33066277e8bf81cd94e4ebf35e4
Benni <Benni>
2023-10-24 14:35:27 +0200

    bitwise Task Benni

---
+++ bitwise.py
@@ -0,0 +1,11 @@
+# 1. convert 4999 to base 2 using python
+# 2. convert 2111 to base 2 using python
+# 3. what will be the value of 4999 & 2111
+# 4. what will be the value of 4999 | 2111
+
+num1 = 4999
+num2 = 2111
+print(bin(num1)) # 0b1001110000111
+print(bin(num2)) # 0b100000111111
+print(bin(num1 & num2)) # 0b111
+print(bin(num1 | num2)) # 0b1101110111111
