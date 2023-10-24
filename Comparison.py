aa8ba22d99c4dff1f58676a938cd5160854cc02e
Benni <Benni>
2023-10-24 13:55:26 +0200

    Comparison Task from Benni

---
+++ Comparison.py
@@ -0,0 +1,17 @@
+var_a = 1500 + 3000
+var_b = 1500 + 3000.00
+var_c = "4500"
+var_d = 4500.000001
+var_e = 1000
+var_f = 5000 - 500
+var_g = 60000
+
+# 1. print out the following values in your script
+print(var_a == var_b)
+print(var_a >= var_b)
+print(var_a > var_b)
+print(var_a > var_c) # will put out an Error cause we can usually not do math with strings in programming
+print(var_a > var_d)
+print(var_g < var_b)
+print(var_a != var_c)
+print(var_e == var_f)
